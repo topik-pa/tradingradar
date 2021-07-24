7159fdad65bfc6d63083829f968c29d1c8c9db61
Marco Pavan <marco.pavan@vivabiocell.it>
2021-07-24 20:04:34 +0200

    Changed prod db admin psw

--- backend/app/configs/db.config.js
+++ backend/app/configs/db.config.js
@@ -1,4 +1,4 @@
 //TODO: url must be taken from env variables
 module.exports = {
-  url: process.env.NODE_ENV === 'test' ? 'mongodb://tr_admin_test:testdb@localhost:27017/tradingradar_test' : process.env.NODE_ENV === 'development' ? 'mongodb://tr_admin_dev:devdb@localhost:27017/tradingradar_dev' : 'mongodb+srv://tr_admin_prod:rxqZOno8YheFURYY@tradingradar.ov6kh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  url: process.env.NODE_ENV === 'test' ? 'mongodb://tr_admin_test:testdb@localhost:27017/tradingradar_test' : process.env.NODE_ENV === 'development' ? 'mongodb://tr_admin_dev:devdb@localhost:27017/tradingradar_dev' : 'mongodb+srv://tr_admin_prod:srjJkPlC4YUMGAoo@tradingradar.ov6kh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
