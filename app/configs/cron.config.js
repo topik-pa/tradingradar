762a1baa1dcfdacc850e53d87229e1f1395b0ec3
Marco Pavan <marco.pavan@vivabiocell.it>
2021-11-30 09:57:21 +0100

    Cron every day at midnight

--- app/configs/cron.config.js
+++ app/configs/cron.config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-let cron = process.env.NODE_ENV === 'test' ? '* */8 * * *' : process.env.NODE_ENV === 'development' ? '*/8 * * * *' : '* */8 * * *'
+let cron = process.env.NODE_ENV === 'test' ? '* */8 * * *' : process.env.NODE_ENV === 'development' ? '*/8 * * * *' : '59 23 * * *'
 module.exports = {
   cron
 }
