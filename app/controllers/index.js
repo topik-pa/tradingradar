507d0b06b32d61ccca07e33219f299f56e7006f7
Marco Pavan <marco.pavan@vivabiocell.it>
2022-01-12 09:50:26 +0100

    Fix allowList + fix string check

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -165,7 +165,7 @@ function getCriteriaObjData (criteria) {
 function toFloatNumber (str) {
   if(str === undefined || str === null) return str
   if(!isNaN(str)) return str
-  if(str instanceof String) {
+  if(typeof str === 'string') {
     let tryNum = Number(parseFloat(str.toString().replace(',', '.').replace('%', '')).toFixed(2))
     if(isNaN(tryNum)) return str
     return tryNum
