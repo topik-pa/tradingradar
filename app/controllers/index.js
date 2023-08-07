9bca35079e2b2987d7064b7f9044c570fc6af655
Marco Pavan <marcopavan.mp@gmail.com>
2023-08-07 14:33:12 +0200

    Sleep 4 secs

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -7,7 +7,7 @@ const availableStockAnalyses = require('../configs/analyses.config')
 const db = require('../models')
 const FTSEMibStock = db.ftseMibStocks
 
-const SLEEP_TIME = 13000
+const SLEEP_TIME = 4000
 
 //COMMON
 function sleep(ms) {
